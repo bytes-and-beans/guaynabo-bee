7dc2ab3f4ae4b3603a1d7480cc6d472715821db6
Wesley Williams <moto42@gmail.com>
2021-10-21 17:20:29 -0500

    added color definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,28 @@ module.exports = {
       },
       colors: {
         guaynabo: {
-          "blue": '#275B72',
-          "blue-dark": '#212B36',
           "gray": '#929496',
+          blue: {
+            DEFAULT: '#275B72',
+            dark: '#212B36',
+            start: '#5fc4be',
+            end: '#275b72',
+          },
+          purple: {
+            DEFAULT: '#eb0072',
+            start: '#eb0072',
+            end: '#862367',
+          },
+          green: {
+            DEFAULT: '#8dc54d',
+            start: '#8dc54d',
+            end: '#016d3b',
+          },
+          orange: {
+            DEFAULT: '#fdc265',
+            start: '#fdc265',
+            end: '#ee5bd2',
+          },
         },
       },
     },

2d8b21c6086a4c433252cf9227b8733c0c0b095f
Wesley Williams <moto42@gmail.com>
2021-12-01 23:21:41 -0600

    stub search function added

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,11 @@
-//Nothing doing yet.
+/**
+ *  This file should contain the 'global' javascript for the app.
+ */
+
+ /** Helper Functions for Alpine */
+
+ /** Send user to the search page for a given search term.
+  */
+ function execute_search(query='') {
+     console.log('called with query ',query);
+ }
